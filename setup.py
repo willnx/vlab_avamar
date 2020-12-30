9c6ff769caac0d11ab40b162b47909cb135c3d2a
willnx <willnx84@gmail.com>
2020-12-30 09:03:08 -0800

    API enforces default values for 'ip-config'

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(name="vlab-avamar-api",
       author="Nicholas Willhite,",
       author_email='willnx84@gmail.com',
-      version='0.0.1',
+      version='2020.12.30',
       packages=find_packages(),
       include_package_data=True,
       package_files={'vlab_avamar_api' : ['app.ini']},
